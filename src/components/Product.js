304874c1a9a6f91bad289e014a8b61fc4acb3e85
pwolaq <pawelwolakk@gmail.com>
2019-06-08 10:51:35 +0200

    Add sort

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tags from "./Tags";
 
-const Product = ({ id, name, tags, onBuyClick }) => {
+const Product = ({ id, name, tags, onBuyClick, price }) => {
     const handleClick = () => onBuyClick(id);
 
     return (
         <div>
-            <span>{name}</span>
-            <Tags items={tags} />
             <button className="btn btn-outline-success float-right" onClick={handleClick}>Buy</button>
+            <h4>{name} <span className="badge badge-secondary">{price}$</span></h4>
+            <Tags items={tags} />
         </div>
     );
 };
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Product from "./Product";
 
+export const SORT_BY_NAME = 'SORT_BY_NAME';
+export const SORT_BY_PRICE = 'SORT_BY_PRICE';
+
+const getSortStrategy = sortBy => {
+    if (sortBy === SORT_BY_NAME) {
+        return (a, b) => a.name.localeCompare(b.name);
+    } else if (sortBy === SORT_BY_PRICE) {
+        return (a, b) => a.price - b.price;
+    }
+
+    return () => 0;
+};
+
 class ProductList extends React.Component {
     state = {
+        sortBy: null,
         products: [
-            {id: 0, name: 'Apple', tags: ['Fruit']},
-            {id: 1, name: 'Coffee', tags: ['Beverage']},
-            {id: 2, name: 'Golden Apple', tags: ['Fruit', 'Valuable']}
+            {id: 0, name: 'Apple', tags: ['Fruit'], price: 15},
+            {id: 1, name: 'Coffee', tags: ['Beverage'], price: 10},
+            {id: 2, name: 'Golden Apple', tags: ['Fruit', 'Valuable'], price: 50},
+            {id: 3, name: 'Banana', tags: ['Fruit'], price: 5},
         ],
     };
 
@@ -17,10 +32,25 @@ class ProductList extends React.Component {
         } : product)
     }));
 
+    handleSortClick = sortBy => this.setState(prevState => {
+        const strategy = getSortStrategy(sortBy);
+
+        return {
+            sortBy,
+            products: prevState.products.sort(strategy)
+        };
+    });
+
     render() {
         return (
             <div className="container">
-                <h1 className="mb-3 mt-3">Product list</h1>
+                <h1 className="mb-3 mt-3">
+                    Product list
+                    <div className="float-right">
+                        <button className="btn btn-success" onClick={() => this.handleSortClick(SORT_BY_NAME)}>Sort by name</button>
+                        <button className="ml-3 btn btn-success" onClick={() => this.handleSortClick(SORT_BY_PRICE)}>Sort by price</button>
+                    </div>
+                </h1>
                 <ul className="list-group">
                     {this.state.products.map(product => (
                         <li className="list-group-item" key={product.id}>
