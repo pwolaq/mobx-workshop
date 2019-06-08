1b69ba671f8a9b89118d29d27b12ec643fd35289
pwolaq <pawelwolakk@gmail.com>
2019-06-08 09:46:58 +0200

    Add state to ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const ProductList = () => (
-    <ul>
-        <li>Apple</li>
-        <li>Coffee</li>
-    </ul>
-);
+class ProductList extends React.Component {
+    state = {
+        products: [
+            {id: 0, name: 'Apple'},
+            {id: 1, name: 'Coffee'},
+        ],
+    };
+
+    render() {
+        return (
+            <ul>
+                {this.state.products.map(({id, name}) => (
+                    <li key={id}>{name}</li>
+                ))}
+            </ul>
+        )
+    }
+}
 
 export default ProductList;
