874acf73bcdca4f8206c189ceb6bc398505c7c33
pwolaq <pawelwolakk@gmail.com>
2019-06-08 12:18:41 +0200

    Switch state to mobx

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {observer} from 'mobx-react';
 import Product from "./Product";
+import ProductStore from '../stores/ProductStore';
 
 export const SORT_BY_NAME = 'SORT_BY_NAME';
 export const SORT_BY_PRICE = 'SORT_BY_PRICE';
@@ -14,51 +16,29 @@ const getSortStrategy = sortBy => {
     return () => 0;
 };
 
+@observer
 class ProductList extends React.Component {
-    state = {
-        sortBy: null,
-        sortReverse: false,
-        searchBy: '',
-        products: [
-            {id: 0, name: 'Apple', tags: ['Fruit'], price: 15},
-            {id: 1, name: 'Coffee', tags: ['Beverage'], price: 10},
-            {id: 2, name: 'Golden Apple', tags: ['Fruit', 'Valuable'], price: 50},
-            {id: 3, name: 'Banana', tags: ['Fruit'], price: 5},
-        ],
+    handleBuyClick = id => ProductStore.products.find(product => product.id === id).isSold = true;
+
+    handleSortClick = sortBy => {
+        ProductStore.sortReverse = ProductStore.sortBy === sortBy ? !ProductStore.sortReverse : false;
+        ProductStore.sortBy = sortBy;
     };
 
-    handleBuyClick = id => this.setState(prevState => ({
-        products: prevState.products.map(product => product.id === id ? {
-            ...product,
-            isSold: true,
-        } : product)
-    }));
-
-    handleSortClick = sortBy => this.setState(prevState => {
-        const sortReverse = prevState.sortBy === sortBy ? !prevState.sortReverse : false;
-
-        return {
-            sortReverse,
-            sortBy
-        }
-    });
-
-    handleSearch = e => this.setState({
-        searchBy: e.currentTarget.value
-    });
+    handleSearch = e => ProductStore.searchBy = e.currentTarget.value;
 
     render() {
-        const sortStrategy = getSortStrategy(this.state.sortBy);
-        const searchBy = this.state.searchBy.toLowerCase();
-        const filteredProducts = searchBy !== '' ? this.state.products.filter(product => product.name.toLowerCase().includes(searchBy)) : this.state.products;
-        const sortedProducts = this.state.sortReverse ? filteredProducts.sort(sortStrategy).reverse() : filteredProducts.sort(sortStrategy);
+        const sortStrategy = getSortStrategy(ProductStore.sortBy);
+        const searchBy = ProductStore.searchBy.toLowerCase();
+        const filteredProducts = searchBy !== '' ? ProductStore.products.filter(product => product.name.toLowerCase().includes(searchBy)) : ProductStore.products;
+        const sortedProducts = ProductStore.sortReverse ? filteredProducts.sort(sortStrategy).reverse() : filteredProducts.sort(sortStrategy);
 
         return (
             <div className="container">
                 <h1 className="mb-3 mt-3 d-flex justify-content-between align-items-center">
                     <div className="d-flex align-items-center">
                         <span>Product list</span>
-                        <input className="ml-3 form-control" style={{ width: 200 }} type="text" value={this.state.searchBy} onChange={this.handleSearch} placeholder="Search..." />
+                        <input className="ml-3 form-control" style={{ width: 200 }} type="text" value={ProductStore.searchBy} onChange={this.handleSearch} placeholder="Search..." />
                     </div>
                     <div>
                         <button className="btn btn-success" onClick={() => this.handleSortClick(SORT_BY_NAME)}>Sort by name</button>
--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -2,11 +2,17 @@ import { observable } from 'mobx';
 
 class ProductStore {
     @observable products = [
-        { id: 0, name: 'Apple', isSold: false },
-        { id: 1, name: 'Coffee', isSold: false },
-        { id: 2, name: 'Strawberry', isSold: false },
-        { id: 3, name: 'Banana', isSold: false },
-    ]
+        {id: 0, name: 'Apple', tags: ['Fruit'], price: 15, isSold: false},
+        {id: 1, name: 'Coffee', tags: ['Beverage'], price: 10, isSold: false},
+        {id: 2, name: 'Golden Apple', tags: ['Fruit', 'Valuable'], price: 50, isSold: false},
+        {id: 3, name: 'Banana', tags: ['Fruit'], price: 5, isSold: false},
+    ];
+
+    @observable sortBy = null;
+
+    @observable sortReverse = false;
+
+    @observable searchBy = '';
 }
 
 const store = new ProductStore();
