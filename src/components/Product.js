89f6c7fbb2c96d6c4af33cdb33366ec544e12743
pwolaq <pawelwolakk@gmail.com>
2019-06-08 11:15:00 +0200

    Add style for bought items

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Tags from "./Tags";
 
-const Product = ({ id, name, tags, onBuyClick, price }) => {
+const Product = ({ id, name, tags, onBuyClick, price, isSold }) => {
     const handleClick = () => onBuyClick(id);
 
     return (
         <div>
             <button className="btn btn-outline-success float-right" onClick={handleClick}>Buy</button>
-            <h4>{name} <span className="badge badge-secondary">{price}$</span></h4>
+            <h4>{isSold ? <s>{name}</s> : name} <span className="badge badge-secondary">{price}$</span></h4>
             <Tags items={tags} />
         </div>
     );
